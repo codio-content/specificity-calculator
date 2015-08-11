69ae6b6821ebb4fc44d25f17b875fff402686c43
mathvdh <m@dcy.io>
2015-08-11 20:42:51 +0000

    hashbang

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # CSS specificity calculator
 
 A tool to calculate the specificity of a css selector.
+
 Forked from : https://github.com/keeganstreet/specificity.keegan.st
-Made responsive and simplified.
+
+Modifications :
+
+- Made responsive via bootstrap.
+- Simplified.
+- Added possibility to pass the selector via hashbang parameter `#rule=``
+(url encode it if it has # signs in it or is too weird)..
+
+
 Licence : MIT
--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+function parseHashBangArgs(aURL) {
+    aURL = aURL || window.location.href;
+
+    var vars = {};
+    var hashes = aURL.slice(aURL.indexOf('#') + 1).split('&');
+
+    for(var i = 0; i < hashes.length; i++) {
+        var hash = hashes[i].split('=');
+
+        if(hash.length > 1) {
+            vars[hash[0]] = hash[1];
+        } else {
+            vars[hash[0]] = null;
+        }      
+    }
+
+    return vars;
+}
+
+
 $(document).ready(function() {
 
 	var $items = $('#items'),
@@ -88,9 +108,22 @@ $(document).ready(function() {
 	};
 
 	$items.empty();
+  
+  if(window.location.hash) {
+                
+    var hashes = parseHashBangArgs();
+    
+    if (hashes.rule) {
+       createItem(decodeURIComponent(hashes.rule));
+    }
+  
+  } else {
+
+    createItem('li:first-child h2 .title');
+  }
     
     
-	createItem('li:first-child h2 .title');
+	
   
   $("a[data-rule]").click(function(){
     $items.empty();
